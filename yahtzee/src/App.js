4f37183eee92e0dc319f5ff9af7ae5c943711620
dd86185 <DietrichDarciJ@JohnDeere.com>
2020-11-14 12:54:56 -0600

    basic set up

---
+++ yahtzee/src/App.js
@@ -0,0 +1,16 @@
+import { Redirect, Route, Switch } from 'react-router';
+import { useSelector, useDispatch} from 'react-redux';
+import { useHistory } from "react-router-dom";
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ yahtzee/src/services/api_helper.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+const api = axios.create({
+    // baseURL:  "https://settlers-of-deere.herokuapp.com",
+    baseURL: "http://localhost:3001"
+})
+
+// ===== AUTH ======
+
+export const signupUser = async (signupData) => {
+    const resp = await api.post("/auth/signup", signupData);
+    console.log(resp.data)
+    localStorage.setItem('authToken', resp.data.token);
+    api.defaults.headers.common.authorization = `Bearer ${resp.data.token}`;
+    return resp.data.user;
+}
+
+export const loginUser = async (loginData) => {
+    const resp = await api.post("/auth/login", loginData);
+    localStorage.setItem("authToken", resp.data.token);
+    api.defaults.headers.common.authorization = `Bearer ${resp.data.token}`;
+    return resp.data.user;
+}
+
+export const verifyUser = async () => {
+    const token = localStorage.getItem("authToken");
+
+    if(token) {
+        api.defaults.headers.common.authorization = `Bearer ${token}`;
+        const resp = await api.get("/auth/verify");
+        return resp.data
+    }
+    return false;
+}
