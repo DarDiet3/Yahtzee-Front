8c221f333b525d49144985f31059523cd21a5e6d
dd86185 <DietrichDarciJ@JohnDeere.com>
2020-11-16 16:14:32 -0600

    basic messages will send

--- yahtzee/src/components/Chat.js
+++ yahtzee/src/components/Chat.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import io from "socket.io-client";
-
+import { socket } from "../services/socketConnection";
 import * as C from "../styles/ChatStyles";
 
 import { currentUser } from "../features/gameMetaDataSlice";
@@ -11,27 +11,49 @@ import { currentUser } from "../features/gameMetaDataSlice";
 
 const Chat = () => {
     const activeUser = useSelector(currentUser);
-    console.log(activeUser)
-
     const [chatData, setChatData] = useState({
         username: activeUser.username,
-        message: "",
+        message: ""
+    })
+    const [chatString, setChatString] = useState({
         messages: []
     })
 
-    const socket = useState(io("http://localhost:3001"))
+    useEffect(() => {
+        const socket = io("http://localhost:3001");
+        socket.on("RECEIVE_MESSAGE", (data) => {
+            addMessage(data)
+        })
+    },[])
+
+    const addMessage = (data) => {
+        let copyMessage = chatString.messages;
+        copyMessage.push(data)
+        const newList ={messages: copyMessage}
+        setChatString(newList)
+    }
 
     const handleChange = (e) => {
         setChatData({...chatData, message: e.target.value})
-        console.log(chatData)
     }
+
+    const sendMessage = (e) => {
+        e.preventDefault();
+        socket.emit("SEND_MESSAGE", {
+            author: chatData.username,
+            message: chatData.message
+        })
+        setChatData({...chatData, message: ""})
+    }
+
+    console.log(chatString.messages)
 
     return(
         <C.ChatDiv>
             <ul id="messages">
-                {chatData.messages.map((message, index) => {
+                {chatString.messages.map((message, index) => {
                     return (
-                        <li>{message.username}: {message.message}</li>
+                        <li>{message.author}: {message.message}</li>
                     )
                 })}
             </ul>
@@ -42,7 +64,7 @@ const Chat = () => {
                     value={chatData.message}
                     onChange={(e) => {handleChange(e)}}
                     />
-                    <C.ChatButton>Send</C.ChatButton>
+                    <C.ChatButton onClick={(e) => sendMessage(e)}>Send</C.ChatButton>
             </C.ChatForm>
         </C.ChatDiv>
     )
